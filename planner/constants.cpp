4a205eb135a68f3f058cf37f4bf4397340e1c47f
rebecca <rebeccarashkin@gmail.com>
2025-02-20 11:42:27 -0700

    first commit, today entry wip

---
+++ planner/constants.cpp
@@ -0,0 +1,15 @@
+#define DAILY_SCHEDULE \
+            <table border="1">\
+                <tr>\
+                    <th>Name</th>\
+                    <th>Age</th>\
+                </tr>\
+                <tr>\
+                    <td>Alice</td>\
+                    <td>25</td>\
+                </tr>\
+                <tr>\
+                    <td>Bob</td>\
+                    <td>30</td>\
+                </tr>\
+            </table>
