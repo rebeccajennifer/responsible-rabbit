80695c171bdae0aced2e9d96849d70c72673ba72
rebecca <rebeccarashkin@gmail.com>
2025-03-08 13:55:41 -0700

    added argument parser

--- planner/python/planner_creation_driver.py
+++ planner/python/planner_creation_driver.py
@@ -1,5 +1,18 @@
+import argparse
 
-from page_layout.half_letter_landscape import HalfLetterSize
+from classes.page_layout.half_letter_landscape import HalfLetterSize
+from classes.planner_parser import PlannerCreationParser
+
+#_______________________________________________________________________
+def new_line (new_line_count: int = 1) -> None:
+  for i in range(new_line_count):
+    print()
 
 if __name__ == '__main__':
+  new_line(2)
   HalfLetterSize.print_something()
+
+  parser: argparse.ArgumentParser = argparse.ArgumentParser()
+  PlannerCreationParser.init_parser(parser)
+
+  args: argparse.Namespace = parser.parse_args()
