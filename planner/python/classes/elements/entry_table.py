cd1658abc680b00df87010edac9ee2543a6f5de2
rebecca <rebeccarashkin@gmail.com>
2025-03-25 18:27:04 -0700

    moved calc column width to utils

--- planner/python/classes/elements/entry_table.py
+++ planner/python/classes/elements/entry_table.py
@@ -37,6 +37,8 @@ from classes.constants.strings import PlannerStrings as Strings
 
 from classes.elements.header_box import HeaderBox
 
+from utils.utils import PlannerUtils as Utils
+
 class EntryTable(svgwrite.container.Group):
 
   DEF_ROW_HGHT: int = 20
@@ -147,7 +149,7 @@ class EntryTable(svgwrite.container.Group):
     # width list does not match the number of columns, make all columns
     # equal.
     #___________________________________________________________________
-    self.col_wdths_: list = self.calc_col_wdths(col_count, col_wdths)
+    self.col_wdths_: list = Utils.calc_col_wdths(self.content_wdth_, col_count, col_wdths)
     #___________________________________________________________________
 
     #___________________________________________________________________
@@ -160,52 +162,6 @@ class EntryTable(svgwrite.container.Group):
     self.add_content()
 
     return
-  #_____________________________________________________________________
-  def calc_col_wdths(self
-    , col_count: int
-    , col_wdths: list
-    ) -> list:
-    """
-    Calculates columns widths.
-
-    Parameters:
-      col_count: Number of columns
-      col_wdths: List of column widths. One element of list should be -1
-                 -1 indicates to span that column to the remaining width
-    """
-
-    #___________________________________________________________________
-    # Column width calculation and error handling
-    #
-    # If no column widths specified, or number of elements in column
-    # width list does not match the number of columns, make all columns
-    # equal.
-    #___________________________________________________________________
-    if ( (not col_wdths) or len(col_wdths) != self.col_count_):
-      col_wdths = col_count * [self.content_wdth_ / col_count]
-
-    # List of col indices to span remaining width
-    span_col_idx: list = []
-    # Running sum of indicated column widths
-    wdth_sum: int = 0
-
-    for i in range(len(col_wdths)):
-      if (col_wdths[i] == -1):
-        span_col_idx = span_col_idx + [i]
-
-      else:
-        wdth_sum = wdth_sum + col_wdths[i]
-
-    span_col_count: int = len(span_col_idx)
-
-    # Replace col width elements that should span remaining space
-    if (span_col_count):
-      remainder_col_wdth: int = (self.content_wdth_ - wdth_sum) / span_col_count
-
-      for i in range(span_col_count):
-        col_wdths[span_col_idx[i]] = remainder_col_wdth
-
-    return col_wdths
 
   #_____________________________________________________________________
   def create_content(self) -> None:
