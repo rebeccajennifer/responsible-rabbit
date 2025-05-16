e4be684a02dd945bd334f03b2d5d840c8d25da2a
rebecca <rebeccarashkin@gmail.com>
2025-05-15 20:47:26 -0700

    updated pdfs

--- planner/layouts/planner-layouts/single-sided/make-pdfs.sh
+++ planner/layouts/planner-layouts/single-sided/make-pdfs.sh
@@ -4,6 +4,7 @@ rsvg-convert -f pdf -o pdf/future-layout.pdf        svg/future-layout.svg
 rsvg-convert -f pdf -o pdf/future-yr-layout.pdf     svg/future-yr-layout.svg
 rsvg-convert -f pdf -o pdf/future-12-wk-layout.pdf  svg/future-12-wk-layout.svg
 rsvg-convert -f pdf -o pdf/goal-layout.pdf          svg/goal-layout.svg
+rsvg-convert -f pdf -o pdf/habit-layout.pdf         svg/habit-layout.svg
 rsvg-convert -f pdf -o pdf/divider-calendar.pdf     svg/divider-calendar.svg
 rsvg-convert -f pdf -o pdf/divider-goals.pdf        svg/divider-goals.svg
 rsvg-convert -f pdf -o pdf/divider-vision.pdf       svg/divider-vision.svg
@@ -18,4 +19,4 @@ rsvg-convert -f pdf -o pdf/divider-week-8.pdf       svg/divider-week-8.svg
 rsvg-convert -f pdf -o pdf/divider-week-9.pdf       svg/divider-week-9.svg
 rsvg-convert -f pdf -o pdf/divider-week-10.pdf      svg/divider-week-10.svg
 rsvg-convert -f pdf -o pdf/divider-week-11.pdf      svg/divider-week-11.svg
-rsvg-convert -f pdf -o pdf/divider-week-12.pdf    svg/divider-week-12.svg
+rsvg-convert -f pdf -o pdf/divider-week-12.pdf      svg/divider-week-12.svg
