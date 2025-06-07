463b63a4a0e090324ed178f5c48b26d1d0bce875
rebecca <rebeccarashkin@gmail.com>
2025-06-07 16:09:12 -0700

    updated pdfs

--- planner/layouts/planner-layouts/single-sided/make-pdfs.sh
+++ planner/layouts/planner-layouts/single-sided/make-pdfs.sh
@@ -1,3 +1,4 @@
+rsvg-convert -f pdf -o pdf/action-items.pdf         svg/action-items.svg
 rsvg-convert -f pdf -o pdf/day-0-layout.pdf         svg/day-0-layout.svg
 rsvg-convert -f pdf -o pdf/day-1-layout.pdf         svg/day-1-layout.svg
 rsvg-convert -f pdf -o pdf/day-2-layout.pdf         svg/day-2-layout.svg
@@ -5,8 +6,9 @@ rsvg-convert -f pdf -o pdf/day-3-layout.pdf         svg/day-3-layout.svg
 rsvg-convert -f pdf -o pdf/day-4-layout.pdf         svg/day-4-layout.svg
 rsvg-convert -f pdf -o pdf/day-5-layout.pdf         svg/day-5-layout.svg
 rsvg-convert -f pdf -o pdf/day-6-layout.pdf         svg/day-6-layout.svg
-rsvg-convert -f pdf -o pdf/habit-layout.pdf         svg/habit-layout.svg
-rsvg-convert -f pdf -o pdf/divider-calendar.pdf     svg/divider-calendar.svg
-rsvg-convert -f pdf -o pdf/divider-goals.pdf        svg/divider-goals.svg
-rsvg-convert -f pdf -o pdf/divider-vision.pdf       svg/divider-vision.svg
+rsvg-convert -f pdf -o pdf/day-6-layout.pdf         svg/day-6-layout.svg
+#rsvg-convert -f pdf -o pdf/habit-layout.pdf         svg/habit-layout.svg
+#rsvg-convert -f pdf -o pdf/divider-calendar.pdf     svg/divider-calendar.svg
+#rsvg-convert -f pdf -o pdf/divider-goals.pdf        svg/divider-goals.svg
+#rsvg-convert -f pdf -o pdf/divider-vision.pdf       svg/divider-vision.svg
 
