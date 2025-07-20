1c135159d31f17931ca399ca898b25b25d4bfba1
rebecca <rebeccarashkin@gmail.com>
2025-07-20 13:48:10 -0700

    added list of emotions

---
+++ planner/python/classes/constants/emotions.py
@@ -0,0 +1,176 @@
+#_______________________________________________________________________
+#_______________________________________________________________________
+#        _   __   _   _ _   _   _   _         _
+#   |   |_| | _  | | | V | | | | / |_/ |_| | /
+#   |__ | | |__| |_| |   | |_| | \ |   | | | \_
+#    _  _         _ ___  _       _ ___   _                    / /
+#   /  | | |\ |  \   |  | / | | /   |   \                    (^^)
+#   \_ |_| | \| _/   |  | \ |_| \_  |  _/                    (____)o
+#_______________________________________________________________________
+#_______________________________________________________________________
+#
+#-----------------------------------------------------------------------
+#   Copyright 2024, Rebecca Rashkin
+#   -------------------------------
+#   This code may be copied, redistributed, transformed, or built
+#   upon in any format for educational, non-commercial purposes.
+#
+#   Please give me appropriate credit should you choose to use this
+#   resource. Thank you :)
+#-----------------------------------------------------------------------
+#
+#_______________________________________________________________________
+#   //\^.^/\\  //\^.^/\\  //\^.^/\\  //\^.^/\\  //\^.^/\\  //\^.^/\\
+#_______________________________________________________________________
+#   DESCRIPTION
+#   List of Brene Brown's 87 emotions.
+#_______________________________________________________________________
+
+class BreneBrownEmotionList:
+  """
+  List of 87 human emotions and experiences from research by Brene Brown
+  """
+
+  PREFIX: str = 'Places we go when...'
+
+  HEADER_UNCERTAIN         : str = 'Things are uncertain or too much'
+  HEADER_COMPARE           : str = 'We compare'
+  HEADER_UNPLANNED         : str = 'Things don\'t go as planned'
+  HEADER_BEYOND_US         : str = 'It\'s beyond us'
+  HEADER_NOT_WHAT_THEY_SEEM: str = 'Things aren\'t what they seem'
+  HEADER_HURT              : str = 'We\'re hurting'
+  HEADER_OTHERS            : str = 'With others'
+  HEADER_FALL_SHORT        : str = 'We fall short'
+  HEADER_CONNECTION        : str = 'We search for connection'
+  HEADER_OPEN              : str = 'The heart is open'
+  HEADER_GOOD              : str = 'Life is good'
+  HEADER_WRONGED           : str = 'We feel wronged'
+  HEADER_SELF_ASSESS       : str = 'To self-assess'
+
+
+
+  UNCERTAIN: list =\
+    [ 'Stress'
+    , 'Overwhelm'
+    , 'Anxiety'
+    , 'Worry'
+    , 'Avoidance'
+    , 'Excitement'
+    , 'Dread'
+    , 'Fear'
+    , 'Vulnerability'
+    ]
+
+  COMPARE: list =\
+    [ 'Comparison'
+    , 'Admiration'
+    , 'Reverence'
+    , 'Envy'
+    , 'Jealousy'
+    , 'Resentment'
+    , 'Schadenfreude'
+    , 'Freudenfreude'
+    ]
+
+  UNPLANNED: list =\
+    [ 'Boredom'
+    , 'Disappointment'
+    , 'Expectations'
+    , 'Regret'
+    , 'Discouragement'
+    , 'Resignation'
+    , 'Frustration'
+    ]
+
+  BEYOND_US: list =\
+    [ 'Awe'
+    , 'Wonder'
+    , 'Confusion'
+    , 'Curiosity'
+    , 'Interest'
+    , 'Surprise'
+    ]
+
+  NOT_WHAT_THEY_SEEM: list =\
+    [ 'Amusement'
+    , 'Bittersweetness'
+    , 'Nostalgia'
+    , 'Cognitive Dissonance'
+    , 'Paradox'
+    , 'Irony'
+    , 'Sarcasm'
+    ]
+
+  HURT: list =\
+    [ 'Anguish'
+    , 'Hopelessness'
+    , 'Despair'
+    , 'Sadness'
+    , 'Grief'
+    ]
+
+  OTHERS: list =\
+    [ 'Compassion'
+    , 'Pity'
+    , 'Empathy'
+    , 'Sympathy'
+    , 'Boundaries'
+    , 'Comparative Suffering'
+    ]
+
+  FALL_SHORT: list =\
+    [ 'Shame'
+    , 'Self-Compassion'
+    , 'Perfectionism'
+    , 'Guilt'
+    , 'Humiliation'
+    , 'Embarrassment'
+    ]
+
+  CONNECTION: list =\
+    [ 'Belonging'
+    , 'Fitting In'
+    , 'Connection'
+    , 'Disconnection'
+    , 'Insecurity'
+    , 'Invisibility'
+    , 'Loneliness'
+    ]
+
+  OPEN: list =\
+    [ 'Love'
+    , 'Lovelessness'
+    , 'Heartbreak'
+    , 'Trust'
+    , 'Self-Trust'
+    , 'Betrayal'
+    , 'Defensiveness'
+    , 'Flooding'
+    , 'Hurt'
+    ]
+
+  GOOD: list =\
+    [ 'Joy'
+    , 'Happiness'
+    , 'Calm'
+    , 'Contentment'
+    , 'Gratitude'
+    , 'Foreboding Joy'
+    , 'Relief'
+    , 'Tranquility'
+    ]
+
+  WRONGED: list =\
+    [ 'Anger'
+    , 'Contempt'
+    , 'Disgust'
+    , 'Dehumanization'
+    , 'Hate'
+    , 'Self-Righteousness'
+    ]
+
+  SELF_ASSESS: list =\
+    [ 'Pride'
+    , 'Hubris'
+    , 'Humility'
+    ]
